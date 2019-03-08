54b6c215060d1e77560197e740f04aa01520ca63
MV <vahyno@gmail.com>
2019-03-07 16:04:27 -0800

    receive initial data

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { handleInitialData } from '../actions/shared';
 
 class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData());
+  }
   render() {
     return (
       <div>Hello World!</div>
@@ -8,4 +13,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default connect()(App);
