92b4fc6f0e35e1ad3a54de452124ca49fa9b8251
MV <vahyno@gmail.com>
2019-03-07 21:01:49 -0800

    loading bar

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -3,16 +3,19 @@ import { getInitialData } from '../utils/api';
 import { receiveTweets } from './tweets';
 import { receiveUsers } from './users';
 import { setAuthedUser } from './authedUser';
+import { showLoading, hideLoading } from 'react-redux-loading-bar';
 
 const AUTHED_ID = "dan_abramov";
 
 export function handleInitialData() {
     return (dispatch) => {
+        dispatch(showLoading());
         return getInitialData()
             .then(({users, tweets})=> {
                 dispatch(receiveTweets(tweets));
                 dispatch(receiveUsers(users));
                 dispatch(setAuthedUser(AUTHED_ID));
+                dispatch(hideLoading());
             });
     }
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,8 +4,11 @@ import authedUser from './authedUser';
 import tweets from './tweets';
 import users from './users'; 
 
+import { loadingBarReducer } from 'react-redux-loading-bar';
+
 export default combineReducers({
     authedUser,
     tweets,
     users,
+    loadingBar: loadingBarReducer,
 })
