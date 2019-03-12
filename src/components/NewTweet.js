d5bbb85a52116e15c07dbcc53da6f19bb3257118
vahyno <vahyno@gmail.com>
2019-03-11 20:46:41 -0700

    New Tweet UI

---
+++ src/components/NewTweet.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class NewTweet extends Component {
+    state = {
+        text: '',
+    }
+
+    handleChange = (e) => {
+        const text = e.target.value;
+        this.setState(()=>({
+            text
+        }))
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const { text } = this.state;
+        const { dispatch, id } = this.props;
+
+        // import handleAddTweet
+
+        this.setState(()=>({
+            text: '',
+        }))
+    }
+
+    render() {
+        const { text } = this.state;
+
+        {/* TODO: redirect to '/' if submitted */}
+
+        const tweetLeft = 280 - text.length;
+        return (
+            <div>
+                <h3 className='center'>Compose new Tweet</h3>
+                <form className='new-tweet' onSubmit={(e)=> this.handleSubmit}>
+                    <textarea 
+                        className='textarea'
+                        maxLength={280}
+                        placeholder="What's happening?"
+                        onChange={this.handleChange}
+                        value={text}
+                    />
+                    {tweetLeft <=100 && (
+                        <div className='tweet-length'>{tweetLeft}</div>
+                    )}
+                    <button 
+                        className='btn'
+                        type='submit'
+                        disabled={text === ''}
+                    >
+                        Submit
+                    </button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default connect()(NewTweet);
+
+
